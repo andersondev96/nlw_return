e59431db110f91a24d2b850df1b65850159e6fad
Anderson Fernandes <anderson.fernandes96@outlook.com>
2022-05-10 22:18:46 -0300

    Creating the backend application

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from 'nodemailer';
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "5d23024ff67ba1",
+    pass: "3fc31a62ffacdb"
+  }
+});
+
+export class NodeMailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: 'Equipe Feedget <oi@feedget.com>',
+      to: 'Anderson Fernandes <andersonfferreira96@gmail.com>',
+      subject,
+      html: body,
+    });
+  }
+}
